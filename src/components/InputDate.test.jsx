2ce428f0c81514e3505faa842417ea2536166e23
ivjose <ivjosesantos@gmail.com>
2021-04-08 19:56:52 +0800

    Added input date unit test

---
+++ src/components/InputDate.test.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '../test-utils/testing-library-utils';
+
+import InputDate from './InputDate';
+
+const TestField = () => {
+  const [value, setValue] = useState('');
+
+  return (
+    <InputDate
+      label="Birthdate"
+      name="birthdate"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+      error="Required fields"
+    />
+  );
+};
+
+test('render input date Field', () => {
+  const { debug } = render(<TestField />);
+
+  const inputDateField = screen.getByLabelText(/birthdate/i);
+
+  debug(inputDateField);
+  expect(inputDateField).toBeInTheDocument();
+
+  userEvent.clear(inputDateField);
+  userEvent.type(inputDateField, '11-21-2001');
+
+  expect(inputDateField.value).toBe('11-21-2001');
+  userEvent.clear(inputDateField);
+  expect(inputDateField.value).toBe('');
+  expect(inputDateField).toHaveClass('border-red-300 text-red-900'); 
+});
