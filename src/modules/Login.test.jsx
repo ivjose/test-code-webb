d9156b265da5ac101a7f87b358e6e56cfca7226d
ivjose <ivjosesantos@gmail.com>
2021-04-08 20:29:35 +0800

    Added test for login

---
+++ src/modules/Login.test.jsx
@@ -0,0 +1,38 @@
+import userEvent from '@testing-library/user-event';
+
+import { render, screen } from '../test-utils/testing-library-utils';
+import Login from './Login';
+
+test.only('render input login', async () => {
+  render(<Login />);
+  const emailField = screen.getByLabelText(/email address/i);
+  expect(emailField).toBeInTheDocument();
+
+  userEvent.type(emailField, 'admin@email.com');
+
+  const passwordField = screen.getByLabelText(/password/i);
+  expect(passwordField).toBeInTheDocument();
+  userEvent.type(passwordField, '12345');
+
+  const submitButton = screen.getByRole('button', { name: /sign in/i });
+  userEvent.click(submitButton);
+});
+
+test('invalid credentials login', () => {
+  const { debug } = render(<Login />);
+  const emailField = screen.getByLabelText(/email address/i);
+  expect(emailField).toBeInTheDocument();
+
+  userEvent.type(emailField, 'other@email.com');
+
+  const passwordField = screen.getByLabelText(/password/i);
+  expect(passwordField).toBeInTheDocument();
+  userEvent.type(passwordField, 'p@ssw0rd');
+
+  const submitButton = screen.getByRole('button', { name: /sign in/i });
+  userEvent.click(submitButton);
+
+  const alert = screen.getByRole('alert');
+  expect(alert).toHaveTextContent(/invalid Credentails/i);
+  debug(emailField);
+});
